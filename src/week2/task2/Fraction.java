4142538739af3d66834a8a54359808801a42eb96
DaoDucKhiem <38343622+DaoDucKhiem@users.noreply.github.com>
2018-09-20 00:10:35 +0700

    nộp bài tập tuần 2 - 17020837

--- src/week2/task2/Fraction.java
+++ src/week2/task2/Fraction.java
@@ -1,30 +1,75 @@
 package week2.task2;
 
-public class Fraction {
+public final class Fraction {
+    private int numerator;
+    private int denominator;
 
+    // setNumerator
+    void setNumerator( int n ) {
+        this.numerator = n;
+    }
+    // setDenominator
+    void setDenominator( int d ) {
+        this.denominator = d;
+    }
+    // getNumerator
+    int getNumerator() {
+        return numerator;
+    }
+    // getDenominator
+    int getDenominator() {
+        return denominator;
+    }
     // TODO: khai báo các thuộc tính
 
     public Fraction(int numerator, int denominator) {
         // TODO: khởi tạo giá trị cho các thuộc tính numberator (tử số), denominator (mẫu số)
+        this.setNumerator(numerator);
+        this.setDenominator(denominator);
     }
 
     public Fraction add(Fraction other) {
         // TODO: Phương thức cộng hai phân số (this và other), trả về đối tượng Fraction mới
-        return null;
+        Fraction add = new Fraction(0,1); // khởi tạo đối tượng để trả về phép cộng
+        int a = this.getDenominator(); // lấy giá trị của mẫu this
+        this.setNumerator( this.getNumerator()* other.getDenominator()); //tử this nhân mẫu other
+        this.setDenominator(a* other.getDenominator()); //mẫu số chung
+        add.setNumerator(this.getNumerator() + a*other.getNumerator()); //cộng tử vs tử
+        add.setDenominator(this.getDenominator());    //mẫu số chung 
+        return add;
     }
 
     public Fraction subtract(Fraction other) {
         // TODO: Phương thức trừ hai phân số (this và other), trả về đối tượng Fraction mới
-        return null;
+        Fraction sub = new Fraction(0,1); // khởi tạo đối tượng để trả về phép trừ
+        int a = this.getDenominator(); // lấy giá trị của mẫu this
+        this.setNumerator( this.getNumerator()* other.getDenominator()); //tử this nhân mẫu other
+        this.setDenominator(a* other.getDenominator()); //mẫu số chung
+        sub.setNumerator(this.getNumerator() - a*other.getNumerator()); //trừ tử vs tử
+        sub.setDenominator(this.getDenominator());    //mẫu số chung
+        return sub;
     }
 
     public Fraction multiply(Fraction other) {
         // TODO: Phương thức nhân hai phân số (this và other), trả về đối tượng Fraction mới
-        return null;
+        Fraction mul = new Fraction(0,1); // khởi tạo đối tượng để trả về phép nhân
+        mul.setNumerator(this.getNumerator()*other.getNumerator()); // nhân tử vs tử
+        mul.setDenominator(this.getDenominator()*other.getDenominator());
+        return mul;
     }
 
     public Fraction divide(Fraction other) {
         // TODO: Phương thức chia hai phân số (this và other), trả về đối tượng Fraction mới
-        return null;
+        Fraction div = new Fraction(0,1); // khởi tạo đối tượng để trả về phép chia
+        div.setNumerator(this.getNumerator()*other.getDenominator()); // nhân tử vs mẫu
+        div.setDenominator(this.getDenominator()*other.getNumerator()); // nhân mẫu với tử
+        return div;
+    }
+    
+    //kiểm tra hai phân số bằng nhau
+    public boolean equals( Fraction other ) {
+        boolean check = false;
+        if( this.getNumerator() == other.getNumerator() && this.getDenominator() == other.getDenominator() ) check = true; 
+        return check;
     }
 }
--- src/week2/task3/Task3.java
+++ src/week2/task3/Task3.java
@@ -4,3 +4,173 @@ public class Task3 {
 }
 
 //TODO: khai báo 3 class tương ứng với 3 đối tượng thực tế ở dưới
+//mèo
+class Cat {
+    private String name;
+    private int age;
+    private String breed;
+    private String color;
+    
+    void setName( String name_ ) {
+        this.name = name_;
+    }
+    
+    void setAge( int age_ ) {
+        this.age = age_;
+    }
+    
+    void setBreed( String breed_ ) {
+        this.breed = breed_;
+    }
+    
+    void setColor( String color_ ) {
+        this.color = color_;
+    }
+    
+    String getName() {
+        return name;
+    }
+    
+    int getAge() {
+        return age;
+    }
+    
+    String getBreed() {
+        return breed;
+    }
+    String getColor() {
+        return color;
+    }
+    
+    // tiếng kêu
+    void Meo() {
+        System.out.println("meoooo....meoooo....");
+    }
+    
+    //xét hai con mèo có cùng giống hay không
+    boolean sameBreed( Cat other ) {
+        return this.getBreed().equals(other.getBreed());
+    }
+    
+    //so sanh tuổi của hai con mèo
+    void ageOfTwoCat( Cat other ) {
+        if(this.getAge()> other.getAge()) System.out.println(">");
+        else if (this.getAge() < other.getAge()) System.out.println("<");
+        else System.out.println("=");
+    }
+}
+
+//class giáo viên
+class Teacher {
+    private String name;
+    private int age;
+    private String Subject;
+    private String married;
+    
+    void setName( String name_ ) {
+        this.name = name_;
+    }
+    
+    void setAge( int age_ ) {
+        this.age = age_;
+    }
+    
+    void setSubject( String sub ) {
+        this.Subject = sub;
+    }
+    
+    void setMarried( String married_ ) {
+        this.married = married_;
+    }
+    
+    String getName() {
+        return name;
+    }
+    
+    int getAge() {
+        return age;
+    }
+    
+    String getSubject() {
+        return Subject;
+    }
+    
+    String getMarried() {
+        return married;
+    }
+    
+    // in ra tên
+    void name() {
+        System.out.println("name: "+this.getName());
+    }
+    
+    //tinh trạng hôn nhân
+    boolean married( String Answer ) {
+        return this.getMarried().equals(Answer);
+    }
+    
+    //xet hai doi tuong teacher có dạy cùng môn không
+    boolean sameSubject( Teacher other ) {
+        return this.getSubject().equals(other.getSubject());
+    }
+    
+}
+
+//class student
+class Student {
+    private String name;
+    private int age;
+    private String id;
+    private String group;
+    
+    void setName( String name_ ) {
+        this.name = name_;
+    }
+    
+    void setAge( int age_ ) {
+        this.age = age_;
+    }
+    
+    void setId( String id ) {
+        this.id = id;
+    }
+    
+    void setGroup( String group_ ) {
+        this.group = group_;
+    }
+    
+    String getName() {
+        return name;
+    }
+    
+    int getAge() {
+        return age;
+    }
+    
+    String getId() {
+        return id;
+    }
+    
+    String getGroup() {
+        return group;
+    }
+    
+    
+    
+    //in ra mã sinh viên
+    void printId() {
+        System.out.println("id: "+this.getId());
+    }
+    
+    //in ra tên sinh viên 
+    void printName() {
+        System.out.println("name: "+ this.getName());
+    }
+    
+    //xem sinh viên có thuộc group không?
+    boolean check( Student other ) {
+        return this.getGroup().equals(other.getGroup());
+    }
+    
+    
+}
