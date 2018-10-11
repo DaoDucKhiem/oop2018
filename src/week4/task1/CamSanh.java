fd9f005afcfa5fbfe0c2e7157d5047baa93d2df4
DaoDucKhiem <38343622+DaoDucKhiem@users.noreply.github.com>
2018-10-11 09:27:43 +0700

    Update CamSanh.java

--- src/week4/task1/CamSanh.java
+++ src/week4/task1/CamSanh.java
@@ -1,6 +1,6 @@
 package week4.task1;
 
-class CamSanh extends Orange {
+public class CamSanh extends Orange {
 	String doDayVo; // độ dày của vỏ
 
     public CamSanh(Double price, Double quantily, String date, String origin, String expirationDate) {
